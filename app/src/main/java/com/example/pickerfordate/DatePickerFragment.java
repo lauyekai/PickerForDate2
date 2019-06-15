6ea004dd1a289399b02c57ceb45cb53782023c42
lauyekai <lauyk-wa15@student.tarc.edu.my>
2019-06-16 01:26:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pickerfordate/DatePickerFragment.java
@@ -0,0 +1,43 @@
+package com.example.pickerfordate;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+
+    public void onDateSet(DatePicker datePicker,
+                          int year, int month, int day){
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processDatePickerResult(year, month, day);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker.
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+// Create a new instance of DatePickerDialog and return it.
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+}
